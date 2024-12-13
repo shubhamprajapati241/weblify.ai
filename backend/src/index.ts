3cc6c58489ba4207b141625d340dae52a6e503ee
shubhamprajapati7748 <shubhamprajapati7748@gmail.com>
2024-12-13 09:25:10 +0530

    updated-backend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,11 @@
 import express from "express";
 import Anthropic from "@anthropic-ai/sdk";
 import { getSystemPrompt } from "./prompts";
-import { DESIGN_PROMPT, reactBasePrompt, nodeBasePrompt } from "./basePrompts";
+import {
+  DESIGN_PROMPT,
+  REACT_BASE_PROMPT,
+  NODE_BASE_PROMPT,
+} from "./basePrompts";
 import { TextBlock } from "@anthropic-ai/sdk/resources";
 import cors from "cors";
 
@@ -11,38 +15,41 @@ app.use(express.json());
 app.use(cors());
 const anthropic = new Anthropic();
 
-app.post("/api/template", async (req, res) => {
-  const userPrompt = req.body.prompt;
-  console.log("userPrompt :", userPrompt);
+app.post("/template", async (req, res) => {
+  const prompt = req.body.prompt;
+
   const response = await anthropic.messages.create({
-    messages: [{ role: "user", content: userPrompt }],
+    messages: [
+      {
+        role: "user",
+        content: prompt,
+      },
+    ],
     model: "claude-3-5-sonnet-20241022",
-    max_tokens: 100,
+    max_tokens: 200,
     system:
-      "Determine if the following message is for React or Node.js. Respond with ONLY one word: 'React', 'Node'. Return NOTHING else",
+      "Return either node or react based on what do you think this project should be. Only return a single word either 'node' or 'react'. Do not return anything extra",
   });
 
   const answer = (response.content[0] as TextBlock).text; // react or node
-  console.log("response :", answer);
-  if (answer == "React") {
+  if (answer == "react") {
     res.json({
       prompts: [
         DESIGN_PROMPT,
-        `Here is an artifact that contains all files of the project visible to you.\nConsider the contents of ALL files in the project.\n\n${reactBasePrompt}\n\nHere is a list of files that exist on the file system but are not being shown to you:\n\n  - .gitignore\n  - package-lock.json\n`,
+        `Here is an artifact that contains all files of the project visible to you.\nConsider the contents of ALL files in the project.\n\n${REACT_BASE_PROMPT}\n\nHere is a list of files that exist on the file system but are not being shown to you:\n\n  - .gitignore\n  - package-lock.json\n`,
       ],
-      uiPrompts: [reactBasePrompt],
+      uiPrompts: [REACT_BASE_PROMPT],
     });
     return;
   }
 
-  if (answer == "Node") {
+  if (answer === "node") {
     res.json({
       prompts: [
-        `Here is an artifact that contains all files of the project visible to you.\nConsider the contents of ALL files in the project.\n\n${reactBasePrompt}\n\nHere is a list of files that exist on the file system but are not being shown to you:\n\n  - .gitignore\n  - package-lock.json\n`,
+        `Here is an artifact that contains all files of the project visible to you.\nConsider the contents of ALL files in the project.\n\n${NODE_BASE_PROMPT}\n\nHere is a list of files that exist on the file system but are not being shown to you:\n\n  - .gitignore\n  - package-lock.json\n`,
       ],
-      uiPrompts: [nodeBasePrompt],
+      uiPrompts: [NODE_BASE_PROMPT],
     });
-
     return;
   }
 
@@ -50,44 +57,18 @@ app.post("/api/template", async (req, res) => {
   return;
 });
 
-app.post("/api/chat", async (req, res) => {
+app.post("/chat", async (req, res) => {
   const messages = req.body.messages;
   const response = await anthropic.messages.create({
     messages: messages,
     model: "claude-3-5-sonnet-20241022",
-    max_tokens: 1000,
+    max_tokens: 8000,
     system: getSystemPrompt(),
   });
   console.log(response);
   res.json({
-    response: (response.content[0] as TextBlock).text,
+    response: (response.content[0] as TextBlock)?.text,
   });
 });
-
-// app.post("/chat", async (req, res) => {
-//   const messages = req.body.messages;
-//   await anthropic.messages
-//     .stream({
-//       messages: [
-//         {
-//           role: "user",
-//           content: messages,
-//         },
-//       ],
-//       model: "claude-3-5-sonnet-20241022",
-//       max_tokens: 200,
-//       system: getSystemPrompt(),
-//     })
-//     .on("text", (text) => {
-//       res.json({
-//         response: text,
-//       });
-
-//       res.json({
-//         response: (response.content[0] as TextBlock)?.text,
-//       });
-//       console.log(text);
-//     });
-// });
 
 app.listen(3000);
